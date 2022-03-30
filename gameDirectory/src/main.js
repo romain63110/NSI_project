de72156ba27512a766a3c7416f4a7b2762a8083e
Greenmattt <greenmattvert@gmail.com>
2022-03-30 16:14:29 +0200

    Merge branch 'main' of https://github.com/romain63110/NSI_project

--- gameDirectory/src/main.js
+++ gameDirectory/src/main.js
@@ -1,25 +1,24 @@
-let config = {
+var config = {
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
     backgroundColor: "AA9900", // Ouais on s'en fout mais si jamais le bgColor se met dans la config et pas dans le create()
     scene: {
         preload: preload,
-        create: create,
-        update: update
+        create: create
     }
 };
 
-let game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
 
 function preload() {
     this.load.spritesheet('greenSquare', 'assets/testSprite.png', { frameWidth: 32, frameHeight: 32 });
 }
 
 function create() {
-    this.add.sprite(0, 0, 'greenSquare');
+    this.add.spritesheet(400, 300, 'greenSquare');
 }
 
-function update() {
+/*function update() {
 
-}
+}*/
