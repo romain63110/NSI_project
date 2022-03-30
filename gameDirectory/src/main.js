04cea21f7de1dc9fdcc7b7b0eb6d6e65cb57a701
Thechalk <ju.lascret@gmail.com>
2022-03-30 15:32:55 +0200

    .

---
+++ gameDirectory/src/main.js
@@ -0,0 +1,25 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    backgroundColor: "AA9900", // Ouais on s'en fout mais si jamais le bgColor se met dans la config et pas dans le create()
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('greenSquare', 'assets/testSprite.png');
+}
+
+function create() {
+    this.add.sprite(0, 0, 'greenSquare');
+}
+
+function update() {
+
+}
