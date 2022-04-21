b919b23c613d7c4405d5bea497596026d3f3de8f
romain63110 <romain.from2005@gmail.com>
2022-04-21 03:27:07 +0200

    Tiles
    TODO:
      -change tiles/images
      -move the character

---
+++ gameDirectory/src/main.js
@@ -0,0 +1,69 @@
+const config = {
+    type: Phaser.AUTO,
+    physics: {
+        default: 'arcade',
+        arcade: {
+        debug: true,
+        gravity: { y: 100 }
+        }
+    },
+    input: {
+        gamepad: true
+    },
+    zoom: 1,
+    pixelArt: true,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+}
+
+const game = new Phaser.Game(config);
+
+let keyboard;
+
+function preload() {
+    //to zoom
+    this.cameras.main.setZoom(this.game.config.zoom);
+
+    //load background image
+    this.load.image('background', './src/assets/images/background.jpg');
+
+    //load player with JSON(to animate)
+    this.load.image('player', './src/assets/images/player.png');
+
+    // load tiles(image)
+    this.load.image('tiles', './src/assets/tiles/images/tiles.png');
+    // load tiles(JSON export)
+    this.load.tilemapTiledJSON('map', './src/assets/tiles/json/tilemap.json'); // tmj ~= json
+
+}
+function create(){
+    //add background                      position   image    origin->position   scale
+    const backgroundImage = this.add.image(0, 0,'background').setOrigin(0, 0).setScale(0.5, 0.5);
+
+    //add player                         position    image
+    this.player = this.physics.add.sprite(50, 300, 'player');
+
+    //add map
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('tiles');
+
+    const platforms = map.createLayer('platform', tileset, 0, 200);//layer platform
+    map.createLayer('creeper', tileset, 0, 200);                   //layer creeper
+
+    // adds colision to tiles 1
+    platforms.setCollision(1); 
+
+
+                                           //tiles where colision is true
+    this.physics.add.collider(this.player, platforms);
+
+}
+function update(){
+
+}
