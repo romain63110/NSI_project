b35d437b02dd97b2c7a89679772ae3c071b295ce
romain63110 <romain.from2005@gmail.com>
2022-05-11 16:18:46 +0200

    debug settings

--- gameDirectory/src/main.js
+++ gameDirectory/src/main.js
@@ -15,8 +15,69 @@ const config = { // configuration du phaser avec les propriétés de bases de ph
                 y: 0.2
             },
             debug: {
+                showAxes: false,
+                showAngleIndicator: true,
+                angleColor: 0xe81153,
+
+                showBroadphase: false,
+                broadphaseColor: 0xffb400,
+
+                showBounds: false,
+                boundsColor: 0xffffff,
+
+                showVelocity: true,
+                velocityColor: 0x00aeef,
+
+                showCollisions: true,
+                collisionColor: 0xf5950c,
+    
+                showSeparations: false,
+                separationColor: 0xffa500,
+
                 showBody: true,
-                showStaticBody: true
+                showStaticBody: true,
+                showInternalEdges: true,
+
+                renderFill: false,
+                renderLine: true,
+    
+                fillColor: 0x106909,
+                fillOpacity: 1,
+                lineColor: 0x28de19,
+                lineOpacity: 1,
+                lineThickness: 1,
+    
+                staticFillColor: 0x0d177b,
+                staticLineColor: 0x1327e4,
+
+                showSleeping: true,
+                staticBodySleepOpacity: 1,
+                sleepFillColor: 0x464646,
+                sleepLineColor: 0x999a99,
+    
+                showSensors: true,
+                sensorFillColor: 0x0d177b,
+                sensorLineColor: 0x1327e4,
+    
+                showPositions: true,
+                positionSize: 4,
+                positionColor: 0xe042da,
+    
+                showJoint: true,
+                jointColor: 0xe0e042,
+                jointLineOpacity: 1,
+                jointLineThickness: 2,
+    
+                pinSize: 4,
+                pinColor: 0x42e0e0,
+    
+                springColor: 0xe042e0,
+    
+                anchorColor: 0xefefef,
+                anchorSize: 4,
+    
+                showConvexHulls: true,
+                hullColor: 0xd703d0
             }
         }
     },
@@ -111,8 +172,10 @@ function create(){
     this.player = this.matter.add.sprite(1*30*16+8*16+16, 2*20*16+18*16-16, 'player');
     this.player.setScale(1) //taille du joueur
     this.player.setFixedRotation() //
-    // this.player.friction = 0;
+    // this.player.friction = 10;
     // this.player.frictionAir = 0.001;
+    // this.player.restitution = 1;
+    // this.player.density = 50;
 
     // animation du joueur
     this.anims.create({
