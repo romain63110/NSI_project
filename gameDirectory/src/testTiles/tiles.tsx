536ad6200de2a12522343a89b78d1d9a8d58ab69
Thechalk <ju.lascret@gmail.com>
2022-05-22 19:46:35 +0200

    .

---
+++ gameDirectory/src/testTiles/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="tiles" tilewidth="16" tileheight="16" tilecount="2072" columns="56">
+ <image source="GitHub/NSI_project/gameDirectory/src/assets/tiles/tilesets.png" width="900" height="600"/>
+</tileset>
